99c6e7c0439c760a3e8e2b41a0afe87d6381bdfc
De Pian, Giuliano <yulomax@gmail.com>
2021-06-25 17:37:00 -0300

    Modificado nombre de funciones, agregada confirmacion (creo que se puede reformular y refactorizar mejor)

--- main.py
+++ main.py
@@ -22,9 +22,13 @@ assistant = AssistantV2(
     authenticator=authenticator
 )
 
+text = ""
+
 
 speech_to_text.set_service_url("https://api.us-south.speech-to-text.watson.cloud.ibm.com/instances/4580bff0-de17-4dcd-a72b-824e5bebe119")
 assistant.set_service_url("https://api.us-south.assistant.watson.cloud.ibm.com/instances/89d698b5-cc02-43df-ae51-6efab4ac629c")
+confirmation = True
+confirmation_awaiting = False
 
 session_response = assistant.create_session(
     assistant_id="a5482507-c0fc-4d95-9ada-ccc695605c05").get_result()
@@ -34,14 +38,14 @@ def greet(message):
     bot.reply_to(message, "Hey! How it's going?")
 
 
-@bot.message_handler(content_types=['voice'])  # Manejador de msg voz
-def hello(message):
+@bot.message_handler(func=lambda message: confirmation == False, content_types=['voice'])  # Manejador de msg voz
+def voice(message):
     audio_info = bot.get_file(message.voice.file_id) # Para descargar/reusar voz
     downloaded_audio = bot.download_file(audio_info.file_path) # Obtener audio file
     text = speech_to_text.recognize(
         audio=downloaded_audio,
         content_type="audio/ogg",
-        model="es-ES_BroadbandModel",
+        model="es-AR_BroadbandModel",
         ).get_result()
     agroresponse = assistant.message(
         assistant_id="a5482507-c0fc-4d95-9ada-ccc695605c05",
@@ -53,6 +57,44 @@ def hello(message):
         ).get_result()
     bot.send_message(message.chat.id, str(text["results"][0]["alternatives"][0]["transcript"]))
     bot.send_message(message.chat.id, str(agroresponse["output"]["generic"][0]["text"]))
+
+
+@bot.message_handler(func=lambda message: confirmation == True and confirmation_awaiting == False, content_types=['voice'])  # Manejador de msg voz
+def voice(message):
+    global confirmation_awaiting
+    confirmation_awaiting = True
+    audio_info = bot.get_file(message.voice.file_id) # Para descargar/reusar voz
+    downloaded_audio = bot.download_file(audio_info.file_path) # Obtener audio file
+    global text
+    text = speech_to_text.recognize(
+        audio=downloaded_audio,
+        content_type="audio/ogg",
+        model="es-AR_BroadbandModel",
+        ).get_result()
+    bot.send_message(message.chat.id, str(text["results"][0]["alternatives"][0]["transcript"]))
+    bot.send_message(message.chat.id, "Es este el mensaje que enviaste? (contesta /si o /no)")
+
+
+@bot.message_handler(func=lambda message: confirmation_awaiting == True, commands=['si'])
+def si(message):
+    global confirmation_awaiting
+    confirmation_awaiting = False
+    agroresponse = assistant.message(
+        assistant_id="a5482507-c0fc-4d95-9ada-ccc695605c05",
+        session_id=str(session_response["session_id"]),
+        input={
+        'message_type': 'text',
+        'text': str(text["results"][0]["alternatives"][0]["transcript"])
+        }
+        ).get_result()
+    bot.send_message(message.chat.id, str(agroresponse["output"]["generic"][0]["text"]))
+
+@bot.message_handler(func=lambda message: confirmation_awaiting == True, commands=['no'])
+def no(message):
+    global confirmation_awaiting
+    confirmation_awaiting = False
+    bot.send_message(message.chat.id, "Envio de mensaje cancelado")
+    pass
 
 
 bot.polling()  # Chequea mensajes continuamente
